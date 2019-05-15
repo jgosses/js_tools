00681c159ac1579e3ef736684d870d3097bc4ee7
Jimmy Gosses <gosses.jim@gmail.com>
2019-05-14 22:38:03 -0400

    import menu toggle images - workshop toggle nav functionality with success

--- pages/jq-nav-proj/pages-js/jq-nav.js
+++ pages/jq-nav-proj/pages-js/jq-nav.js
@@ -1,6 +1,15 @@
 //messing around with jquery, emphasis on finding elements at first
 $('header').addClass('wow');
-$('li:lt(4)').hide().fadeIn(1500);
+$('li:lt(5)').hide();//.fadeIn(3000);
 $('li').on('click', function() {
-    $(this).remove();
+    $(this).hide();
 });
+
+//toggle navlist on hamburger click
+$('.menu-click').on('click', function() {
+    $('li:lt(5)').toggle();
+    //replace hamburger icon with x using z-index
+    $('.menu-imgclose').toggle();
+    $('.menu-img').toggle();
+});
+
